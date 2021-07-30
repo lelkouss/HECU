6470e60d963ff37e545e577b8eab139cacdf32cc
MatthewCastilleja <mcastilleja2002@gmail.com>
2021-07-30 13:24:32 -0500

    organized script file

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-let room, tiles0, borders0, player, rooms;
+let currentRoom, tiles0, borders0;
+let player, rooms, spawners, enemies;
+
+//SPRITES
 let spriteFloor, spriteWall;
 let spriteBorderBottom, spriteBorderTop, spriteBorderRight, spriteBorderLeft; 
+
+// PRELOAD ALL SPRITES AND MUSIC
+function preload() {
+  spriteFloor = loadImage("/assets/tile.png");
+  spriteWall = loadImage("/assets/wall.png");
+  spriteBorderBottom = loadImage("/assets/borderbottom.png");
+  spriteBorderTop = loadImage("/assets/bordertop.png");
+  spriteBorderRight = loadImage("/assets/borderright.png");
+  spriteBorderLeft = loadImage("/assets/borderleft.png");
+}
+
 
 function setup() {
   displayScale = 3;
@@ -11,15 +25,9 @@ function setup() {
   canvasBuffer.noSmooth();
   colorMode(HSB, 360, 100, 100);
   
-  spriteFloor = loadImage("/assets/tile.png");
-  spriteWall = loadImage("/assets/wall.png");
-  spriteBorderBottom = loadImage("/assets/borderbottom.png");
-  spriteBorderTop = loadImage("/assets/bordertop.png");
-  spriteBorderRight = loadImage("/assets/borderright.png");
-  spriteBorderLeft = loadImage("/assets/borderleft.png");
-
-
-  level = [];
+  rooms = [];
+  spawners = [];
+  enemies = [];
   //init room
   tiles0 = [ [0, 0, 0, 0, 0, 0, 0],
             [0, 1, 0, 0, 0, 1, 0],
@@ -31,16 +39,16 @@ function setup() {
   borders0 = ["A_1", "B_1", "C_1", "D_1"];
 
   //sets up Levels (individual rooms included in a single array)
-  level.push(new Room(0, tiles0, borders0));
-  room = level[0];
+  rooms.push(new Room(0, tiles0, borders0));
+  currentRoom = rooms[0];
   
   //init player
-  player = new Player(25*3, 20*6, room);
+  player = new Player(25*3, 20*6, currentRoom);
 }
 
 function draw() {
   background(0);
-  room.display();
+  currentRoom.display();
   player.update();
   scale(displayScale);
   image(canvasBuffer,0,0);
