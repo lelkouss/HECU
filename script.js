be4128d7d7ffbdf90c9a3db4407bde988693f983
MatthewCastilleja <mcastilleja2002@gmail.com>
2021-07-30 13:59:50 -0500

    did stuff

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let currentRoom, tiles0, borders0;
-let player, rooms, spawners, enemies;
+let player, rooms, spawners, enemies, bullets;
 
 //SPRITES
 let spriteFloor, spriteWall;
@@ -25,31 +25,35 @@ function setup() {
   canvasBuffer.noSmooth();
   colorMode(HSB, 360, 100, 100);
   
-  rooms = [];
-  spawners = [];
-  enemies = [];
-  //init room
-  tiles0 = [ [0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 1, 0],
-            [0, 0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0],
-            [0, 1, 0, 0, 0, 1, 0],
-            [0, 0, 0, 0, 0, 0, 0] ];
-  borders0 = ["A_1", "B_1", "C_1", "D_1"];
+  // THIS WILL BE MOVED TO A DIFFERENT CLASS LATER
+  tiles0 = [  [0, 0, 0, 0, 0, 0, 0],
+              [0, 1, 0, 0, 0, 1, 0],
+              [0, 0, 0, 1, 0, 0, 0],
+              [0, 0, 0, 1, 0, 0, 0],
+              [0, 0, 0, 1, 0, 0, 0],
+              [0, 1, 0, 0, 0, 1, 0],
+              [0, 0, 0, 0, 0, 0, 0] ];
+  borders0 =  ["A_1", "B_1", "C_1", "D_1"];
 
-  //sets up Levels (individual rooms included in a single array)
-  rooms.push(new Room(0, tiles0, borders0));
-  currentRoom = rooms[0];
   
-  //init player
+  // set up the arrays for the current state of the game
+  rooms = [new Room(0, tiles0, borders0)];
+  currentRoom = rooms[0];
+  spawners = []; //currentRoom.spawners;
+  //currentSpawner = spawners[0];
+  enemies = []; //currentSpawner.enemies;
   player = new Player(25*3, 20*6, currentRoom);
+  bullets = [];
 }
 
 function draw() {
   background(0);
   currentRoom.display();
   player.update();
+  //update enemies
+  //update bullets
+
+
   scale(displayScale);
   image(canvasBuffer,0,0);
 }
