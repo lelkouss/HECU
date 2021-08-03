4edce2620bb2dac4ece99edab5f556c755985632
lelkouss <88159124+lelkouss@users.noreply.github.com>
2021-08-03 09:35:10 -0400

    Update enemy.js

--- enemy.js
+++ enemy.js
@@ -1,124 +1,140 @@
 //move/health is dependent on inheritance - To be implemented
 
 class Enemy {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.width = 10;
-        this.height = 10;
-        //this.type = type;
-        //this.health = 10; //needs to be dependent on type - inheritance
-        this.shootCoolDown = 120;
+  constructor(x, y) {
+      this.x = x;
+      this.y = y;
+      this.width = 10;
+      this.height = 10;
+      //this.type = type;
+      //this.health = 10;
+      this.shootCoolDown = 120;
 
-        //this.colorMap = {0: [0, 100, 100], 1:[120, 100, 100], 2:[240, 100, 100]};
-    }
+      //this.colorMap = {0: [0, 100, 100], 1:[120, 100, 100], 2:[240, 100, 100]};
+  }
 
-    update() {     
-        this.display();
-        this.shootCoolDown--;
-        //shoots every 2 seconds
-        if(this.shootCoolDown < 0) {
-            this.shoot();
-            this.shootCoolDown = 120;
-        }
-        
-    }
+  update() {     
+      this.display();
+      this.shootCoolDown--;
+      //shoots every 2 seconds
+      if(this.shootCoolDown < 0) {
+          this.shoot();
+          this.shootCoolDown = 120;
+      }
+      
+  }
 
-    display() {
-        //canvasBuffer.fill(this.colorMap[this.type]);
-        canvasBuffer.rect(this.x, this.y, this.width, this.height);
-    }
+  display() {
+      //canvasBuffer.fill(this.colorMap[this.type]);
+      canvasBuffer.rect(this.x, this.y, this.width, this.height);
+  }
 
-    shoot() {
-        let playerVector = createVector( (player.x+player.width/2) - (this.x+this.width/2), (player.y+player.height/2) - (this.y+this.height/2));
-        playerVector.normalize();
+  shoot() {
+      let playerVector = createVector( (player.x+player.width/2) - (this.x+this.width/2), (player.y+player.height/2) - (this.y+this.height/2));
+      playerVector.normalize();
 
-        let new_bullet = new Bullet(this.x+this.width/2, this.y+this.height/2, playerVector.mult(4.5), 1);
-        bullets.push(new_bullet);
-    }
+      let new_bullet = new Bullet(this.x+this.width/2, this.y+this.height/2, playerVector.mult(4.5), 1);
+      bullets.push(new_bullet);
+  }
 
-    shot() {
-        this.health -= 5;
-        if(this.health <= 0) {
-            enemies.splice(enemies.indexOf(this), 1);
-        }
-    }
+  shot() {
+      this.health -= 5;
+      if(this.health <= 0) {
+          enemies.splice(enemies.indexOf(this), 1);
+      }
+  }
 }
 
 class Roomba extends Enemy{
-    constructor(x, y) {
-        super(x, y);
-        this.health = 10;
-      }
-
-      update() {   
-        this.move();  
-        super.display();
-        super.shootCoolDown--;
-        //shoots every 2 seconds
-        if(super.shootCoolDown < 0) {
-          super.shoot();
-          super.shootCoolDown = 120;
-        }
-        
+  constructor(x, y) {
+      super(x, y);
+      this.health = 10;
+      this.velocityX = 0;
+      this.velocityY = 0;
     }
 
-      move() {
-        let oldX = this.x;
-        let oldY = this.y;
-        
-        //random movement for roomba
-        this.x = this.x + random(-5, 5);
-        this.y = this.y + random(-5, 5);
+    update() {   
+      this.move();  
+      super.display();
+      super.shootCoolDown--;
+      //shoots every 2 seconds
+      if(super.shootCoolDown < 0) {
+        super.shoot();
+        super.shootCoolDown = 120;
+      }
+      
+  }
 
-        // check if collided with a wall on the x axis
-        for(let i=0; i<currentRoom.tiles.length; i++) {
-          for(let j=0; j<currentRoom.tiles[i].length; j++) {
-            if(currentRoom.tiles[i][j] == 1 && collideRectRect(this.x, this.y, this.width, this.height, j*currentRoom.tileWidth+currentRoom.borderOffset, i*currentRoom.tileHeight+currentRoom.borderOffset, currentRoom.tileWidth, currentRoom.tileHeight)) {
-              this.x = oldX;
-              break;
-            } 
-          }
+    move() {
+      let oldX = this.x;
+      let oldY = this.y;
+      this.velocityX = 1;
+      this.velocityY = 1;
+      //random movement for roomba
+      this.x = this.x;
+      this.y = this.y;
+        /*if (this.x < 0) {
+          dirX *= -1
+        }  
+        if (this.x > width) {
+          dirX *= -1
+        }  
+        if (this.y < 0) {
+          dirY *= -1
         }
+        if (this.y > height) {
+          dirX *= -1
+        }  */
         
-        // check if colldied with player on x axis
-          if(collideRectRect(this.x, this.y, this.width, this.height, player.x, player.y, player.width, player.height)) {
+      // check if collided with a wall on the x axis
+      for(let i=0; i<currentRoom.tiles.length; i++) {
+        for(let j=0; j<currentRoom.tiles[i].length; j++) {
+          if(currentRoom.tiles[i][j] == 1 && collideRectRect(this.x, this.y, this.width, this.height, j*currentRoom.tileWidth+currentRoom.borderOffset, i*currentRoom.tileHeight+currentRoom.borderOffset, currentRoom.tileWidth, currentRoom.tileHeight)) {
             this.x = oldX;
-          }
-
-        // check if collided with a wall on the y axis
-        for(let i=0; i<currentRoom.tiles.length; i++) {
-          for(let j=0; j<currentRoom.tiles[i].length; j++) {
-            if(currentRoom.tiles[i][j] == 1 && collideRectRect(this.x, this.y, this.width, this.height, j*currentRoom.tileWidth+currentRoom.borderOffset, i*currentRoom.tileHeight+currentRoom.borderOffset, currentRoom.tileWidth, currentRoom.tileHeight)) {
-              this.y = oldY;
-              break;
-            }
-          }
+            break;
+          } 
         }
-        
-        // check if colldied with player on y axis
-          if(collideRectRect(this.x, this.y, this.width, this.height, player.x, player.y, player.width, player.height)) {
-            this.y = oldY;
-          }
-
-  
-        // check if collided with a border on the x axis
-        if(this.x < currentRoom.borderOffset || this.x + this.width > currentRoom.width-currentRoom.borderOffset) {
+      }
+      
+      // check if colldied with player on x axis
+        if(collideRectRect(this.x, this.y, this.width, this.height, player.x, player.y, player.width, player.height)) {
           this.x = oldX;
         }
-        //check if collided with a border on the y axis
-        if(this.y < currentRoom.borderOffset || this.y + this.height > currentRoom.height-currentRoom.borderOffset) {
+
+      // check if collided with a wall on the y axis
+      for(let i=0; i<currentRoom.tiles.length; i++) {
+        for(let j=0; j<currentRoom.tiles[i].length; j++) {
+          if(currentRoom.tiles[i][j] == 1 && collideRectRect(this.x, this.y, this.width, this.height, j*currentRoom.tileWidth+currentRoom.borderOffset, i*currentRoom.tileHeight+currentRoom.borderOffset, currentRoom.tileWidth, currentRoom.tileHeight)) {
+            this.y = oldY;
+            break;
+          }
+        }
+      }
+      
+      // check if colldied with player on y axis
+        if(collideRectRect(this.x, this.y, this.width, this.height, player.x, player.y, player.width, player.height)) {
           this.y = oldY;
         }
 
-        
+
+      // check if collided with a border on the x axis
+      if(this.x < currentRoom.borderOffset || this.x + this.width > currentRoom.width-currentRoom.borderOffset) {
+        this.x = oldX;
       }
-  
+      //check if collided with a border on the y axis
+      if(this.y < currentRoom.borderOffset || this.y + this.height > currentRoom.height-currentRoom.borderOffset) {
+        //dirY  = dirY * -1;
+        this.y = oldY;
+      }
+
+      
+    }
+
 }
 class Turret extends Enemy{
-    constructor(x, y) {
-        super(x, y);
-        this.health = 15;
-      }
+  constructor(x, y) {
+      super(x, y);
+      this.health = 15;
+    }
 
 }
